2259c6af3dff89e96fd67761766ff589ff5e89c1
willhwj <williamhuwj@gmail.com>
2021-11-26 00:00:12 +0800

    added CRUD to room table and HBS files

--- routes/rooms.js
+++ routes/rooms.js
@@ -28,10 +28,8 @@ router.get('/create', async(req, res)=> {
 router.post('/create', async (req, res)=> {
     const allRoomTypes = await Room_type.fetchAll().map(roomType => {
         return [roomType.get('id'), roomType.get('name')];
-    })
-    
+    })  
     const roomForm = createRoomForm(allRoomTypes);
-    console.log(roomForm);
     roomForm.handle(req, {
         'success': async (form)=> {
             const room = new Room();
@@ -47,6 +45,78 @@ router.post('/create', async (req, res)=> {
             })
         }
     })
+})
+
+// update rooms
+router.get('/:room_id/update', async(req, res) => {
+    const roomId = req.params.room_id;
+    const room = await Room.where({
+        'id': roomId
+    }).fetch({
+        require: true
+    });
+    const allRoomTypes = await Room_type.fetchAll().map(roomType => {
+        return [roomType.get('id'), roomType.get('name')];
+    });
+    const roomForm = createRoomForm(allRoomTypes);
+    roomForm.fields.room_number.value = room.get('room_number');
+    roomForm.fields.room_price.value = room.get('room_price');
+    roomForm.fields.room_type_id.value = room.get('room_type_id');
+    
+    res.render('rooms/update', {
+        'form': roomForm.toHTML(bootstrapField),
+        'room': room.toJSON()
+    })
+})
+
+router.post('/:room_id/update', async(req, res) => {
+    const roomId = req.params.room_id;
+    const room = await Room.where({
+        'id': roomId
+    }).fetch({
+        require: true
+    });
+    const allRoomTypes = await Room_type.fetchAll().map(roomType => {
+        return [roomType.get('id'), roomType.get('name')]
+    });
+    const roomForm = createRoomForm(allRoomTypes);
+    roomForm.handle(req, {
+        'success': async(form)=> {
+            room.set(form.data);
+            room.save();
+            res.redirect('/rooms');
+        },
+        'error': async(form)=> {
+            res.render('rooms/updates',{
+                'form': form.toHTML(bootstrapField),
+                'room': room.toJSON()
+            })
+        }
+    })
+})
+
+// delete a room
+router.get('/:room_id/delete', async (req, res) => {
+    const roomId = req.params.room_id;
+    const room = await Room.where({
+        'id': roomId
+    }).fetch({
+        require: true
+    });
+    res.render('rooms/delete', {
+        'room': room.toJSON()
+    })
+})
+
+router.post('/:room_id/delete', async (req, res) => {
+    const roomId = req.params.room_id;
+    const room = await Room.where({
+        'id': roomId
+    }).fetch({
+        require: true
+    });
+    await room.destroy();
+    res.redirect('/rooms');
 })
 
 module.exports = router;
