65cc0430c36ae558181b990f5331911a6d1b0b4c
willhwj <williamhuwj@gmail.com>
2021-12-14 20:41:50 +0800

    added logic to update room type inventory when a new room is created

--- routes/rooms.js
+++ routes/rooms.js
@@ -5,11 +5,12 @@ const { Room, Room_type} = require('../models');
 // import in checkIfAuthenticated middleware
 const { checkIfAuthenticated} = require('../middleware');
 // import in the DAL
-const dataLayer = require('../dal/rooms');
+const dataLayerRooms = require('../dal/rooms');
+const dataLayerRoomTypes = require('../dal/room-types');
 
 // display rooms
 router.get('/', checkIfAuthenticated, async(req, res) =>{
-    let rooms = await dataLayer.getAllRooms();
+    let rooms = await dataLayerRooms.getAllRooms();
     res.render('rooms/index', {
         'rooms': rooms.toJSON()
     })
@@ -17,7 +18,7 @@ router.get('/', checkIfAuthenticated, async(req, res) =>{
 
 // create rooms
 router.get('/create', checkIfAuthenticated, async(req, res)=> {
-    const allRoomTypes = await dataLayer.getAllRoomTypes();
+    const allRoomTypes = await dataLayerRoomTypes.getAllRoomTypes();
     const roomForm = createRoomForm(allRoomTypes);
     res.render('rooms/create', {
         'form': roomForm.toHTML(bootstrapField)
@@ -25,7 +26,7 @@ router.get('/create', checkIfAuthenticated, async(req, res)=> {
 });
 
 router.post('/create', checkIfAuthenticated, async (req, res)=> {
-    const allRoomTypes = await dataLayer.getAllRoomTypes(); 
+    const allRoomTypes = await dataLayerRoomTypes.getAllRoomTypes(); 
     const roomForm = createRoomForm(allRoomTypes);
     roomForm.handle(req, {
         'success': async (form)=> {
@@ -34,6 +35,16 @@ router.post('/create', checkIfAuthenticated, async (req, res)=> {
             room.set('room_price', form.data.room_price);
             room.set('room_type_id', form.data.room_type_id);
             await room.save();
+            // increase inventory of room type by 1
+            const roomType = await Room_type.where({
+                'id': form.data.room_type_id
+            }).fetch({
+                require: true
+            });
+            let currentInv = roomType.get('inventory');
+            currentInv ++;
+            roomType.set('inventory', currentInv);
+            await roomType.save();
             res.redirect('/rooms');
         },
         'error': async(form) => {
@@ -47,8 +58,8 @@ router.post('/create', checkIfAuthenticated, async (req, res)=> {
 // update rooms
 router.get('/:room_id/update', checkIfAuthenticated, async(req, res) => {
     const roomId = req.params.room_id;
-    const room = await dataLayer.getRoomById(roomId);
-    const allRoomTypes = await dataLayer.getAllRoomTypes();
+    const room = await dataLayerRooms.getRoomById(roomId);
+    const allRoomTypes = await dataLayerRoomTypes.getAllRoomTypes();
     const roomForm = createRoomForm(allRoomTypes);
     roomForm.fields.room_number.value = room.get('room_number');
     roomForm.fields.room_price.value = room.get('room_price');
@@ -62,8 +73,8 @@ router.get('/:room_id/update', checkIfAuthenticated, async(req, res) => {
 
 router.post('/:room_id/update', checkIfAuthenticated, async(req, res) => {
     const roomId = req.params.room_id;
-    const room = await dataLayer.getRoomById(roomId);
-    const allRoomTypes = await dataLayer.getAllRoomTypes();
+    const room = await dataLayerRooms.getRoomById(roomId);
+    const allRoomTypes = await dataLayerRoomTypes.getAllRoomTypes();
     const roomForm = createRoomForm(allRoomTypes);
     roomForm.handle(req, {
         'success': async(form)=> {
@@ -83,7 +94,7 @@ router.post('/:room_id/update', checkIfAuthenticated, async(req, res) => {
 // delete a room
 router.get('/:room_id/delete', checkIfAuthenticated, async (req, res) => {
     const roomId = req.params.room_id;
-    const room = await dataLayer.getRoomById(roomId);
+    const room = await dataLayerRooms.getRoomById(roomId);
     res.render('rooms/delete', {
         'room': room.toJSON()
     })
@@ -91,7 +102,7 @@ router.get('/:room_id/delete', checkIfAuthenticated, async (req, res) => {
 
 router.post('/:room_id/delete', checkIfAuthenticated, async (req, res) => {
     const roomId = req.params.room_id;
-    const room = await dataLayer.getRoomById(roomId);
+    const room = await dataLayerRooms.getRoomById(roomId);
     await room.destroy();
     res.redirect('/rooms');
 })
